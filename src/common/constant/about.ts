4ea99d14df0589a3551bc1ab19a4f96bae60454e
Göktuğ Ceyhan <goktugceyhan@gmail.com>
2024-07-13 15:36:30 +0300

    chore: update sitemap URLs and last modified dates

--- src/common/constant/about.ts
+++ src/common/constant/about.ts
@@ -6,7 +6,7 @@ export const ABOUT = `
 	I'm Göktuğ. With over 3+ years of experience in Web Development, covering both frontend and backend, I'm well-versed in Backend technologies like PHP & NodeJS. I'm particularly passionate about backend side, where I focus on creating web applications that are not only precise but also user-friendly experiences.
 </p>
 <p>
-	I specialize in JavaScript and PHP, also it's frameworks and databases, including MySQL , PostgreSQL , <br/> Supabase, Node.js, and PHP (Laravel). I love searching new web technologies and features.
+	I specialize in JavaScript and PHP, also it's frameworks and databases, including MySQL , PostgreSQL, Supabase, Node.js, and PHP (Laravel). I love searching new web technologies and features.
 </p>
 <p>
 	I firmly believe that effective communication is vital for success in any role.
