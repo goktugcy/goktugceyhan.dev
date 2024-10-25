df4f2f78544b3f2ab0d13a5865f29b49753772ea
Göktuğ Ceyhan <goktugceyhan@gmail.com>
2024-10-25 10:52:06 +0300

    refactor: Update error handling in API endpoints
    Update the error handling in the API endpoints for fetching available devices and now playing data. Added try-catch blocks to handle any errors that may occur during the API calls. If an error occurs, a JSON response with an error message is returned. This ensures better error handling and provides more informative responses to the client.

--- src/services/spotify.ts
+++ src/services/spotify.ts
@@ -15,87 +15,96 @@ const AVAILABLE_DEVICES_ENDPOINT = `${BASE_URL}/me/player/devices`;
 const NOW_PLAYING_ENDPOINT = `${BASE_URL}/me/player/currently-playing`;
 
 export const getAvailableDevices = async (): Promise<DeviceResponseProps> => {
-  const { data } = await supabase
-    .from('spotify_token')
-    .select('token')
-    .order('created_at', { ascending: false })
-    .limit(1);
+  try {
+    const { data } = await supabase
+      .from('spotify_token')
+      .select('token')
+      .order('created_at', { ascending: false })
+      .limit(1);
 
-  const token = data?.[0]?.token || '';
+    const token = data?.[0]?.token || '';
 
-  const response = await axios.get(AVAILABLE_DEVICES_ENDPOINT, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+    const response = await axios.get(AVAILABLE_DEVICES_ENDPOINT, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-  const status = response.status;
+    const status = response.status;
 
-  if (status === 204 || status > 400) {
-    return { status, data: [] };
+    if (status === 204 || status > 400) {
+      return { status, data: [] };
+    }
+
+    const responseData: DeviceDataProps = response.data;
+
+    const devices = responseData?.devices?.map((device) => ({
+      name: device.name,
+      is_active: device.is_active,
+      type: device.type,
+      model: PAIR_DEVICES[device?.type]?.model || 'Unknown Device',
+      id: PAIR_DEVICES[device?.type]?.id || 'goktugcy-device',
+    }));
+
+    return {
+      status,
+      data: devices,
+    };
+  } catch (error) {
+    return { status: 500, data: [] };
   }
-
-  const responseData: DeviceDataProps = response.data;
-
-  const devices = responseData?.devices?.map((device) => ({
-    name: device.name,
-    is_active: device.is_active,
-    type: device.type,
-    model: PAIR_DEVICES[device?.type]?.model || 'Unknown Device',
-    id: PAIR_DEVICES[device?.type]?.id || 'goktugcy-device',
-  }));
-
-  return {
-    status,
-    data: devices,
-  };
 };
 
 export const getNowPlaying = async (): Promise<NowPlayingResponseProps> => {
-  const { data } = await supabase
-    .from('spotify_token')
-    .select('token')
-    .order('created_at', { ascending: false })
-    .limit(1);
+  try {
+    const { data } = await supabase
+      .from('spotify_token')
+      .select('token')
+      .order('created_at', { ascending: false })
+      .limit(1);
 
-  const token = data?.[0]?.token;
-  const response = await axios.get(NOW_PLAYING_ENDPOINT, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+    const token = data?.[0]?.token;
+    const response = await axios.get(NOW_PLAYING_ENDPOINT, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-  const status = response.status;
+    const status = response.status;
 
-  if (status === 204 || status > 400) {
-    return { status, isPlaying: false, data: null };
+    if (status === 204 || status > 400) {
+      return { status, isPlaying: false, data: null };
+    }
+
+    const responseData: SongProps = response.data;
+
+    if (!responseData.item) {
+      return { status, isPlaying: false, data: null };
+    }
+
+    const isPlaying: boolean = responseData?.is_playing;
+    const album: string = responseData?.item?.album.name ?? '';
+    const albumImageUrl: string | undefined =
+      responseData?.item?.album?.images?.find((image) => image?.width === 640)
+        ?.url ?? undefined;
+    const artist: string =
+      responseData?.item?.artists?.map((artist) => artist?.name).join(', ') ??
+      '';
+    const songUrl: string = responseData?.item?.external_urls?.spotify ?? '';
+    const title: string = responseData?.item?.name ?? '';
+
+    return {
+      status,
+      isPlaying,
+      data: {
+        album,
+        albumImageUrl,
+        artist,
+        songUrl,
+        title,
+      },
+    };
+  } catch (error) {
+    return { status: 500, isPlaying: false, data: null };
   }
-
-  const responseData: SongProps = response.data;
-
-  if (!responseData.item) {
-    return { status, isPlaying: false, data: null };
-  }
-
-  const isPlaying: boolean = responseData?.is_playing;
-  const album: string = responseData?.item?.album.name ?? '';
-  const albumImageUrl: string | undefined =
-    responseData?.item?.album?.images?.find((image) => image?.width === 640)
-      ?.url ?? undefined;
-  const artist: string =
-    responseData?.item?.artists?.map((artist) => artist?.name).join(', ') ?? '';
-  const songUrl: string = responseData?.item?.external_urls?.spotify ?? '';
-  const title: string = responseData?.item?.name ?? '';
-
-  return {
-    status,
-    isPlaying,
-    data: {
-      album,
-      albumImageUrl,
-      artist,
-      songUrl,
-      title,
-    },
-  };
 };
