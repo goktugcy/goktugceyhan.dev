70974fb320190ee02230a706b3d7c20b3e75193b
Göktuğ Ceyhan <goktugceyhan@gmail.com>
2025-09-04 21:02:46 +0300

    Refactor code structure for improved readability and maintainability

--- src/modules/home/components/Home.tsx
+++ src/modules/home/components/Home.tsx
@@ -2,11 +2,14 @@ import Breakline from '@/common/components/elements/Breakline';
 
 import Introduction from './Introduction';
 import Services from './Services';
+import CareerList from '../../about/components/CareerList';
 import Skills from '../../about/components/Skills';
 const Home = () => {
   return (
     <>
       <Introduction />
+      <Breakline className='mt-8 mb-7' />
+      <CareerList />
       <Breakline className='mt-8 mb-7' />
       <Skills />
       <Breakline className='mt-8 mb-7' />
